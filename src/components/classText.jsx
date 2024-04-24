ad70ffd2f10739c85b048a264de852637800d7db
Nicole Arnold <nicocobei@gmail.com>
2024-04-24 20:37:45 +0200

    first commit

---
+++ src/components/classText.jsx
@@ -0,0 +1,6 @@
+import React from "react";
+export default class ClassText extends React.Component {
+  render() {
+    return <p>Class Component Text</p>;
+  }
+}
---
+++ src/components/funcText.jsx
@@ -0,0 +1,3 @@
+export default function FuncText() {
+  return <p>Function Component Text</p>;
+}
